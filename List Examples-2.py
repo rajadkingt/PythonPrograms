9117a205f313516149daaf1ee5c0f505b1df5436
Raja King <rajadkingt@gmail.com>
2024-06-09 19:33:26 -0500

    updated examples 2 again

--- List Examples-2.py
+++ List Examples-2.py
@@ -15,3 +15,14 @@ print(list1)
 
 list1.sort(reverse=True)
 print(list1)
+
+'''this will add the list but not its element'''
+list2=[7,9,11,12]
+list1.append(list2)
+print(list1) #[6, 5, 3, 2, 2, 1, [7, 9, 11, 12]]
+
+''' do this to add list elements to list1'''
+list1.pop()
+print(list1)
+list1.extend(list2)
+print(list1) #[6, 5, 3, 2, 2, 1, 7, 9, 11, 12]
