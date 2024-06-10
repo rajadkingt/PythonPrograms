7c006501b10ec4c03d8a03f99d7451b16e8b2297
Raja King <rajadkingt@gmail.com>
2024-06-09 19:20:21 -0500

    added examples 2

---
+++ List Examples-2.py
@@ -0,0 +1,15 @@
+
+list1=list(range(1,500,7))
+print(list1)
+
+''' clear a list'''
+list1.clear()
+print(list1)
+list1=[1,2,2,2,3,4,5,6]
+
+'''remove the first occurance of a value'''
+list1.remove(2)
+print(list1)
+list1.remove(4)
+print(list1)
+
